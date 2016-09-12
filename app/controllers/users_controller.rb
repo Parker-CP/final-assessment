131298661302fb6911a292df024e494e156d6320
Parker P <exxcaelum@gmail.com>
2016-09-12 10:17:02 -0600

    add ability to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UserController < ApplicationController
     @user = current_user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash.now[:error] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
 end
