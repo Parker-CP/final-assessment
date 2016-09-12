07e2ba387674d7ecd11914c129906d8fdd6910fb
Parker P <exxcaelum@gmail.com>
2016-09-12 10:23:39 -0600

    add user params, create html page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
   def new
     @user = User.new
@@ -17,6 +17,12 @@ class UserController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
