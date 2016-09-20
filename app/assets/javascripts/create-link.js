3d909e0fac8d1d8535468707b727cb57aa171212
Parker P <exxcaelum@gmail.com>
2016-09-20 15:00:47 -0600

    create links

---
+++ app/assets/javascripts/create-link.js
@@ -0,0 +1,29 @@
+function createLink(){
+  $('#submit-link').on('click', function(){
+    var title = $('#link-title').val();
+    var url = $('#link-url').val();
+    var data = {title: title, url: url};
+    $.ajax({
+      method: 'POST',
+      url: '/api/v1/links',
+      dataType: "JSON",
+      data: data,
+      success: function(link){
+        resetFields();
+        insertLink(link);
+      },
+      error: function(data){
+        alert("Please enter valid data");
+      }
+    });
+  });
+}
+
+function resetFields(){
+  $('#link-title').val('');
+  $('#link-url').val('');
+}
+
+function insertLink(link){
+  $('#links').prepend(buildLink(link));
+}
--- app/assets/javascripts/thought-box.js
+++ app/assets/javascripts/thought-box.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
   buildLinks();
   alphabetize();
+  createLink();
 });
