bd1a5c03d7492cc5027b379dc6e1b0e9d93461a8
Parker P <exxcaelum@gmail.com>
2016-09-20 15:10:12 -0600

    update read status of links

---
+++ app/assets/javascripts/reading.js
@@ -0,0 +1,29 @@
+function reading(){
+  $(document).on('click', '.markRead', function(event){
+    var postId = $(this).parent().data('postId');
+    $.ajax({
+      method: "PATCH",
+      url: "/api/v1/links/" + postId + "/read",
+      success: read(postId)
+    });
+  });
+
+  $(document).on('click', '.markUnread', function(event){
+    var postId = $(this).parent().data('postId');
+    $.ajax({
+      method: "PATCH",
+      url: "/api/v1/links/" + postId + "/unread",
+      success: unread(postId)
+    });
+  });
+}
+
+function read(id){
+  $('#' + id).children('span').text("Read: true");
+  $('#' + id).addClass('read-back');
+}
+
+function unread(id){
+  $('#' + id).children('span').text("Read: false");
+  $('#' + id).children('a').removeClass('read-back');
+}
--- app/assets/javascripts/thought-box.js
+++ app/assets/javascripts/thought-box.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
   buildLinks();
   alphabetize();
   createLink();
+  reading();
 });
