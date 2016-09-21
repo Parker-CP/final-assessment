ecb81e78fb5a90ebb92545eee8ccbf78cefc7dc7
Parker P <exxcaelum@gmail.com>
2016-09-20 22:27:00 -0600

    password confirmation working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :links
 
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates_confirmation_of :password
 end
